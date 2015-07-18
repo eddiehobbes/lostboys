2d735518804b80024858ced5eeebd88c1d5ccf37
Edward Yee <osuyee.82@gmail.com>
2015-07-18 18:31:56 -0400

    working on gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
+
 var gulp = require('gulp');
-
-//Deletes files
 var del = require('del');
-
-//Injects file references into your index.html
 var inject = require('gulp-inject')
 var angularFilesort = require('gulp-angular-filesort')
-
-//Loads files
 var plugins = require('gulp-load-plugins');
-
-//Create source maps
-var sourcemaps = require('gulp-sroucemaps');
+// var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var eslint = require('gulp-eslint');
 
 var paths = {
     scripts: 'src/**/*.js',
-    styles: ['./src/**/*.css', './src/**/*.scss'],
     home: './src/fossilized/',
     targetIndex: './src/fossilized/index.html',
     build: './build',
-    dist: './dist/'
+    sass: './src/resources/**/*.scss',
+    finalCSS: './serve/',
+    dist: './dist/',
+    serveTarget: './serve/'
 };
 
 gulp.task('index', function() {
@@ -28,9 +28,8 @@ gulp.task('index', function() {
         .pipe(angularFilesort());
 
     return target.pipe(inject(sources))
-        .pipe(gulp.dest(paths.distDev));
+        .pipe(gulp.dest(paths.serveTarget));
 });
-
 
 gulp.task('javascript', function() {
     gulp.src(paths.scripts)
@@ -39,35 +38,52 @@ gulp.task('javascript', function() {
         .pipe(gulp.dest(paths.distDev));
 });
 
+gulp.task('js', function() {
+    gulp.src(paths.scripts)
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.distDev));
+});
+
 //browserify
 
 //Build css from sass
+gulp.task('sass', function() {
+  return gulp.src(paths.sass)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.finalCSS))
+});
+
 
 //Need to lint
+gulp.task('lint', function() {
+    return gulp.src([paths.scripts])
+    .pipe(eslint())
+    .pipe(eslint.format());
+})
 
 //Need to uglify and rename .min
 
 //Need to clean build
 
 //Need to run watches
+gulp.task('js-watch', ['js'], browserSync.reload);
 
-//Need to serve page
+//Need to start server
+
+//Need to reload on file changes
+gulp.task('serve', ['lint', 'index', 'sass'], function() {
+    browserSync.init({
+      server: {
+        baseDir: paths.serveTarget
+        }
+    });
+
+    gulp.watch(paths.scripts, ['js-watch']);
+
+});
 
 //Need to run tests
+gulp.task('test', ['lint'], function() {
 
-
-
-// gulp.src('./src/app/index.html')
-//     .pipe(inject(
-//         gulp.src(['.src/app/**/*.js'])
-//         .pipe(angularFilesort())
-//     ))
-//     .pipe(gulp.dest('./build'));
-
-// gulp.task('clean-dev' , function(callback) {
-//     del([
-//         paths.distDev
-//     ], callback);
-// });
-//
-// gulp.task('default', ['clean-dev']);
+});
---
+++ src/fossilized/lostboys.js
@@ -0,0 +1,13 @@
+// 'use strict';
+// (function(angular) {
+//
+//     angular.module('lostboys', [
+//     ]).config(config);
+//
+//     function config() {
+//
+//     }
+// })(angular);
+(function() {
+    console.log('here we go');
+})()
